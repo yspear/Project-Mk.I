9c60e3ef221d1c9d4a2e98dafc1465debc4288e2
yspear <yspear3@gmail.com>
2024-09-26 00:05:24 +0700

    Add files via upload

--- place-holder/character_body_2d.gd
+++ place-holder/character_body_2d.gd
@@ -2,16 +2,28 @@ extends Area2D
 
 
 var max_speed := 600.0
+var normal_speed := 600
+var dash_speed := 1800
 var velocity := Vector2(0,0)
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("go_left", "go_right")
 	direction.y = Input.get_axis("go_up", "go_down")
-
+	#movemnt code
+	if Input.is_action_just_pressed("dash"):
+		max_speed = dash_speed
+		get_node("Dash_cooldown").start()
 	velocity = direction * max_speed
+	#Dashing
 	position += velocity * delta
 	if direction.length() > 0.0:
 		direction = direction.normalized()
+	#to make the movement smoother
 func _physics_process(delta: float) -> void:
 	look_at(get_global_mouse_position())
+	#make so player is pointing at mouse
+
+
+func _on_dash_cooldown_timeout() -> void:
+	max_speed = normal_speed
